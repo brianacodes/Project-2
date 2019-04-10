6e53043e9172bd910c200a2ab8b68ab9c8204dba
Tudor Bejinari <tudorbejinari@gmail.com>
2019-04-09 19:18:20 -0600

    do it

---
+++ Project-2/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+  {
+    name: "Read more, Watch more tv : https://www.netflix.com/browse/genre/839338",
+    photo:
+      "https://images.unsplash.com/photo-1464052259809-c19ec2ddfc5f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80",
+    scores: ["5", "1", "1", "5", "5", "1", "1", "5", "5", "4"]
+  },
+  {
+    name: "It's time to get a pet https://www.ddfl.org/adoption/adopt-a-working-cat/?gclid=Cj0KCQjw1pblBRDSARIsACfUG11fpaOcdJ6xBwEkHcH3GxhMUih9u-k9c7oJ9JRL_V3fbn87K-1qdHkaAsp2EALw_wcB",
+    photo:
+      "https://images.unsplash.com/photo-1550943247-c51407364bd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80",
+    scores: ["5", "5", "5", "5", "5", "1", "5", "1", "5", "1"]
+  },
+  {
+    name: "Be around your friends! Fall in love again! https://www.datingforseniors.com/",
+    photo:
+      "https://images.unsplash.com/photo-1494438043283-22a3c46831a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    scores: ["5", "1", "1", "5", "5", "5", "5", "1", "3", "1"]
+  },
+  {
+    name: "Go out! Spend more time with your friends. These is the best club: http://coclubs.com/the-church/",
+    photo: "https://images.unsplash.com/photo-1497129907035-91f1b95c8119?ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "1", "4", "3"]
+  },
+  {
+    name: "You definitely need a partner https://seniordatefinder.com/",
+    photo: "https://images.unsplash.com/photo-1490349708435-19d432984978?ixlib=rb-1.2.1&auto=format&fit=crop&w=1487&q=80",
+    scores: ["1", "1", "5", "5", "4", "1", "1", "4", "1", "5"]
+  },
+  {
+    name: "Perfect books https://seniordatefinder.com/",
+    photo: "https://images.unsplash.com/photo-1490349708435-19d432984978?ixlib=rb-1.2.1&auto=format&fit=crop&w=1487&q=80",
+    scores: ["1", "1", "5", "5", "4", "1", "1", "4", "1", "5"]
+  }
+];
+module.exports = friends;
