e4d1bb136c0d7f9a93b2d30a2b7305af78c604fd
unknown <exumcodes@gmail.com>
2019-04-11 02:32:09 -0600

    changes to images and img folder structure; edits to homepage content (index.html - PART 3)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
 	});
 
 	app.get('/survey', function (req, res) {
-	  	res.sendFile(path.join(__dirname, "../public/views/survey.html"));
+		res.sendFile(path.join(__dirname, "../public/views/survey.html"));
   });
   
   app.get('/solutions', function (req, res) {
